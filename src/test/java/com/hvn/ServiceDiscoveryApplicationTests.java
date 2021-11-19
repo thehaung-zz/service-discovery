5e3a4d1100fa919e7c4bdba698cae5666ab0d423
Hau Nguyen <thehaung217@gmail.com>
2021-11-19 13:42:42 +0700

    Initial commit

---
+++ src/test/java/com/hvn/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
