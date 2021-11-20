5d62c638b08e7ae5545c6aaf7c159fd63c936b7b
Hau Nguyen <thehaung217@gmail.com>
2021-11-20 09:34:44 +0700

    chore: remove unused file

--- build.sh
+++
@@ -1,4 +0,0 @@
-docker build -t service-discovery .
-docker stop service-discovery
-docker rm service-discovery
-docker run -d --restart=always -p 8761:8761 --name service-discovery -t service-discovery
